e4d6449045e2f1778fc20d98b4fee13da830f0ac
Aleksi Hirvensalo <aleksi.hirvensalo@aalto.fi>
2021-01-23 11:49:38 +0200

    Pagination implemented with React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
+import './App.css'
+
+import Category from './components/category'
 
 function App() {
+
+  //All the possible categories are listed here. To add a new category, simply add it here. 
+  const categories = ['beanies', 'facemasks', 'gloves']
+
   return (
-    <div className="App">
-      Content here
-    </div>
+      <Router>
+
+        <div className='menu'>
+          {
+          categories.map(category => {
+            return <Link key={category} to={'/' + category}>{category}</Link> //Create link to each categorys own "page". 
+          })}
+        </div>
+
+        <Switch>
+          {
+          categories.map(category => { //Map categories to routes. 
+            return (
+            <Route path={'/' + category}>
+              <Category name={category}/>
+            </Route>)
+          })}
+        </Switch>
+        
+      </Router>
   );
 }
 
