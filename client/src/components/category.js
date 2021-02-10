90d1f6646e47c26ed4813a46273a36e75377fd1e
Aleksi Hirvensalo <aleksi.hirvensalo@aalto.fi>
2021-02-10 22:42:52 +0200

    Added error messages and loader. Implement coloured circles to tell item amounts in stock.

--- client/src/components/category.js
+++ client/src/components/category.js
@@ -1,31 +1,61 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Alert from '@material-ui/lab/Alert'
 
-import Item from './item'
+import InfiniteList from './infiniteList'
+
+import './../css/category.css'
 
 //Component for displaying items of certain category. Takes category name as a parameter. 
 const Category = ({name}) => {
 
     const [items, setItems] = useState([])
 
+    const [errorMsg, setErrorMsg] = useState('')
+
     //Attempts to get data from url and set items to data if succesful. 
     useEffect(() => {
+
+        console.log('Fetching data from: /' + name)
+    
         axios.get(name)
-            .then(response => {                
-                console.log('succesfully fetched data from: /' + name)
-                console.log(response)
+            .then(response => {
+                console.log('Succesfully fetched data from: /' + name)
                 setItems(response.data)
-        })
-            .catch(error => {
-                setItems('Failed to get data from: /' + name)
+                setErrorMsg('')
             })
+            .catch(error => {
+                console.log(error)
+                if(error.response.status === 503) //Server responds with 503, if it could not reach the APIs or parsing data threw error. 
+                    setErrorMsg(error.response.data)
+                else
+                    setErrorMsg('Failed to get data from: /' + name)
+            })  
+
     }, [name])
+
+    //Returns list of items to render or error/loader depending on the situation.  
+    const getList = () => {
+
+        //If there are error messages, render error. Otherwise items (if found) or loader.  
+        if(errorMsg !== '') {
+            return (
+            <Alert className='error' severity='error'>
+                {errorMsg + ' - try again by refreshing the page!'}
+            </Alert>
+            )
+        } else if (items.length !== 0){ 
+            return <InfiniteList elements={items} />
+        } else { 
+            return(
+            <LinearProgress className='loader' /> 
+        )}
+    }
 
     return(
         <div className='category'>
-            {items.map(item => {
-                return <Item key={item.id} name={item.name} manufacturer={item.manufacturer} />
-            })}
+            {getList()}
         </div>
     )
 }
--- client/src/components/item.js
+++ client/src/components/item.js
@@ -1,11 +1,41 @@
 import React from 'react'
 
+import './../css/category.css'
+
 //Component for displaying a single item. 
-const Item = ({name, manufacturer}) => {
+const Item = ({name, manufacturer, available}) => {
+
+    //Checks if item is in stock. Return corresponding circle colour. 
+    const circleColour = () => {
+        let circle
+    
+        switch(available) {
+            case 'instock': 
+                circle = 'green-circle'
+                break;
+            case 'less than 10':
+                circle = 'yellow-circle'
+                break;
+            default:
+                circle = 'red-circle'
+        }
+
+        return circle
+    } 
+
     return(
-        <div className='item'>
-            {name}
-            {manufacturer}
+        <div className='infinite-item'>
+
+            <div className='infinite-item--info'>
+                <div> <b>Name: </b>{name.toLowerCase()} </div>
+                <div> <b>Manufacturer: </b>{manufacturer} </div>
+            </div>
+
+            <div className='infinite-item--stock'>
+                {available}
+                <div className={circleColour()}></div>
+            </div>
+            
         </div>
     )
 }
